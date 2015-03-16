6f3558e24da72042622ce62df3a2a7af2e36109e
Daniel Steele <danielsteele@hotmail.co.uk>
2015-03-16 13:26:41 +0000

    Removed unnecessary about page

--- app/main/config/routes.rb
+++ app/main/config/routes.rb
@@ -1,6 +1,5 @@
 # See https://github.com/voltrb/volt#routes for more info on routes
 
-get '/about', _action: 'about'
 
 # Routes for login and signup, provided by user-templates component gem
 get '/signup', _controller: 'user-templates', _action: 'signup'
